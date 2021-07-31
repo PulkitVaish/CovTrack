0e5591e80b6163c2362f4b49db8d9ace63ac69d5
pkulkarni245 <72600670+pkulkarni245@users.noreply.github.com>
2021-07-31 19:48:53 +0530

    Footer Update
    Co-Authored-By: Pulkit Vaish <69940487+PulkitVaish@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -182,7 +182,7 @@ const App = () => {
       <div className="bottomNav">
           <div>
             <Typography className="navText">
-              Made with Precaution by <a href="http://pkulkarni245.github.io" target="0">Pavan</a> and <a href="https://www.linkedin.com/in/pulkit-vaish/" target="0">Pulkit</a>
+              Made with Precaution by <a href="https://www.linkedin.com/in/pavan-kulkarni-a9b433201/" target="0">Pavan</a> and <a href="https://www.linkedin.com/in/pulkit-vaish/" target="0">Pulkit</a>
             </Typography>
           </div>
       </div>
